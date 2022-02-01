61ec5c5dd093cb1f9a55b62dbf638214c9728e0e
wahoojustin <justin.mitchell@wahoofitness.com>
2022-02-01 11:46:26 -0500

    Change random seed

--- statistics/normal_examples.py
+++ statistics/normal_examples.py
@@ -6,6 +6,6 @@ import os
 
 os.chdir("pytools/statistics")
 
-np.random.seed(555)  # Ensures repeatable arrival at the same random data
+np.random.seed(0)  # Ensures repeatable arrival at the same random data
 data = np.random.normal(20, 5, size=500)  # Generate normally distributed random data
 fit_normal(data, plot=True, save_as="normal_examples_plot_fit.png")  # Fit that data
